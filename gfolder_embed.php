bbf186831706a0ab92308796c732bcd27280e269
Tom Woodward <twwoodward@me.com>
2015-03-22 21:52:04 -0400

    Initial publishing
    added the grid/list parameter to the shortcode

---
+++ gfolder_embed.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Plugin Name: Embed a Google Drive Folder w the unique folder ID 
+ * Plugin URI: http://bionicteaching.com
+ * Description: Allows an embed of the Google Drive Folder via the unique ID. You can also define a list or grid view via the style parameter.
+ * Version: 1.1
+ * Author: Tom Woodward
+ * Author URI: http://bionicteaching.com
+ * License: GPL2
+ */
+ 
+ /*   2014  PLUGIN_AUTHOR_NAME  (email : bionicteaching@gmail.com)
+ 
+ 	This program is free software; you can redistribute it and/or modify
+ 	it under the terms of the GNU General Public License, version 2, as 
+ 	published by the Free Software Foundation.
+ 
+ 	This program is distributed in the hope that it will be useful,
+ 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ 	GNU General Public License for more details.
+ 
+ 	You should have received a copy of the GNU General Public License
+ 	along with this program; if not, write to the Free Software
+ 	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+ 
+//[gdrive id="YourFolderID" style="Grid or List"]
+ 
+function gdrive_shortcode($atts, $content=null) {
+    $a = shortcode_atts( array(
+         'id' => '',
+         'style' => 'list',
+    ), $atts); 
+	return '<iframe src="https://drive.google.com/embeddedfolderview?id=' . $a['id'] . '#' . $a['style'] . '" frameborder="0" width="100%" height="500px" scrolling="auto"> </iframe>';
+}
+add_shortcode( 'gdrive', 'gdrive_shortcode' );
+?>
