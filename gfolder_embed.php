52d545e712e6deb3608ffd2557f630831d9eb7e0
woodwardtw <twwoodward@me.com>
2022-09-02 08:58:39 -0400

    address resource key issue
    from https://github.com/woodwardtw/gfolder_embed/issues/5

--- gfolder_embed.php
+++ gfolder_embed.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Embed a Google Drive Folder w the unique folder ID 
  * Plugin URI: https://github.com/woodwardtw/gfolder_embed
- * Description: Embed your Google Drive Folder via the unique ID. You can also define a list or grid view via the style parameter. [gdrive id="YourFolderID" style="grid or list" width="" height=""]
+ * Description: Embed your Google Drive Folder via the unique ID. You can also define a list or grid view via the style parameter. [gdrive id="YourFolderID" style="grid or list" width="" height="" resourcekey="may be needed in some cases"]
  * Version: 1.1
  * Author: Tom Woodward
  * Author URI: http://bionicteaching.com
@@ -11,32 +11,40 @@
  
  /*   2014  PLUGIN_AUTHOR_NAME  (email : bionicteaching@gmail.com)
  
- 	This program is free software; you can redistribute it and/or modify
- 	it under the terms of the GNU General Public License, version 2, as 
- 	published by the Free Software Foundation.
+     This program is free software; you can redistribute it and/or modify
+     it under the terms of the GNU General Public License, version 2, as 
+     published by the Free Software Foundation.
  
- 	This program is distributed in the hope that it will be useful,
- 	but WITHOUT ANY WARRANTY; without even the implied warranty of
- 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- 	GNU General Public License for more details.
+     This program is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     GNU General Public License for more details.
  
- 	You should have received a copy of the GNU General Public License
- 	along with this program; if not, write to the Free Software
- 	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+     You should have received a copy of the GNU General Public License
+     along with this program; if not, write to the Free Software
+     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
  
-//[gdrive id="YourFolderID" style="[grid OR list]" width="" height=""]
+//[gdrive id="YourFolderID" style="Grid or List" width="" height=""]
  
 function gdrive_shortcode($atts, $content=null) {
-	// Normalize attribute keys, lowercase
-    $atts = array_change_key_case( (array)$atts, CASE_LOWER );
-
+    $rkey = '';
     $a = shortcode_atts( array(
          'id' => '',
          'style' => 'list',
          'width' => '100%',
          'height' => '500px',
+         'resourcekey' => '',
+         'sort' => ''
     ), $atts); 
-	return '<iframe src="https://drive.google.com/embeddedfolderview?id=' . $a['id'] . '#' . $a['style'] . '" frameborder="0" width="' . $a['width'] . '" height="' . $a['height'].'" scrolling="auto"> </iframe>';
+   
+     if($a['resourcekey']){
+        $rkey = '&resourcekey=' .$a['resourcekey'];
+    }
+    $id = $a['id'];
+    $style = $a['style'];
+    $width = $a['width'];
+    $height = $a['height'];
+     return "<iframe src='https://drive.google.com/embeddedfolderview?id={$id}{$rkey}#{$style}' frameborder='0' width='{$width}' height='{$height}' scrolling='auto'></iframe>";
 }
 add_shortcode( 'gdrive', 'gdrive_shortcode' );
